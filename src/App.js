a4e42aca763061ae2c30fb0c1dfda4ab30156c9e
Hossain-Sajjad <sunshinems58.com>
2022-05-24 05:34:51 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../src/Pages/Home/Home'
 import Header from './Pages/Shared/Header';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
-    <div>
+    <div className='app'>
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>} />
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -3,29 +3,30 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div class="navbar bg-base-100">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <li><Link to="/">Purchase</Link></li>
                         <li><Link to="/">Blog</Link></li>
                         <li><Link to="/">My Portfolio</Link></li>
                     </ul>
                 </div>
-                <Link to="/" class="btn btn-ghost normal-case text-xl">daisyUI</Link>
+                <Link to="/" className="lg:flex hidden btn btn-ghost normal-case text-xl">Hossain Manufacturing Ltd.</Link>
+                <Link to="/" className="lg:hidden btn btn-ghost normal-case text-xl">Hossain Manufacturing</Link>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
                     <li><Link to="/">Purchase</Link></li>
                     <li><Link to="/">Blog</Link></li>
                     <li><Link to="/">My Portfolio</Link></li>
                 </ul>
             </div>
-            <div class="navbar-end">
-                <Link to="/" class="btn">Login</Link>
+            <div className="navbar-end">
+                <Link to="/" className="btn">Login</Link>
             </div>
         </div>
     );
