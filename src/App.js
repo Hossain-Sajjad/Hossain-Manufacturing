6ee03220e1b2e4b59ebb5e7a3cbb6a8a8d3c4c2e
Hossain-Sajjad <sunshinems58.com>
2022-05-28 04:06:41 +0600

    add tools page created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from '../src/Pages//Home/Home'
 import Header from './Pages/Shared/Header';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Login/Register';
+import AddTools from './Pages/AddProduct/AddTools';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/home" element={<Home></Home>} />
         <Route path="/login" element={<Login></Login>} />
         <Route path="/register" element={<Register></Register>} />
+        <Route path="/tool" element={<AddTools></AddTools>} />
       </Routes>
     </div>
   );
