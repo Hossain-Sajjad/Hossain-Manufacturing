b4f6bae09d7f0e05df8c190cb79f402e87f16951
Hossain-Sajjad <sunshinems58.com>
2022-05-29 07:43:30 +0600

    update profile created

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,9 +1,108 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../firebase.init';
 
 const MyProfile = () => {
+    const [user] = useAuthState(auth);
+    const [profile, setProfile] = useState('');
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    useEffect(() => {
+        const url = `http://localhost:5000/userprofile?email=${user.email}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setProfile(data))
+    }, [user])
+    console.log(profile);
+    const onSubmit = data => {
+        data.email = user.email;
+        data.name = user.displayName;
+        console.log(data);
+        fetch('http://localhost:5000/userprofile', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
     return (
         <div>
-            <h2>My profile</h2>
+            <div class="hero my-8 bg-base-200">
+                <div class="hero-content text-center">
+                    <div class="max-w-md">
+                        <h1 class="text-2xl font-bold">Name: {user.displayName}</h1>
+                        <p class="py-6">Email: {user.email}</p>
+                        {profile[0]?.education && <p class="py-6">Education: {profile[0]?.education}</p>}
+                        {profile[0]?.location && <p class="py-6">Location: {profile[0]?.location}</p>}
+                        {profile[0]?.phone && <p class="py-6">Phone: {profile[0]?.phone}</p>}
+                        {profile[0]?.socialLink && <p class="py-6">Social Link: {profile[0]?.socialLink}</p>}
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="hero  bg-base-200">
+                    <div class="hero-content flex-col lg:flex-row-reverse">
+                        <div class="text-center lg:text-left">
+                            <h1 class="text-3xl font-bold">Update Your Profile!</h1>
+                        </div>
+                        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                            <div class="card-body">
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <div className="form-control w-full max-w-xs">
+                                        <label className="label">
+                                            <span className="label-text">Education</span>
+                                        </label>
+                                        <input
+                                            type="education"
+                                            placeholder="Education"
+                                            className="input input-bordered w-full max-w-xs"
+                                            {...register("education")}
+                                        />
+                                    </div>
+                                    <div className="form-control w-full max-w-xs">
+                                        <label className="label">
+                                            <span className="label-text">Location</span>
+                                        </label>
+                                        <input
+                                            type="location"
+                                            placeholder="Location"
+                                            className="input input-bordered w-full max-w-xs"
+                                            {...register("location")}
+                                        />
+                                    </div>
+                                    <div className="form-control w-full max-w-xs">
+                                        <label className="label">
+                                            <span className="label-text">Phone</span>
+                                        </label>
+                                        <input
+                                            type="phone"
+                                            placeholder="Phone"
+                                            className="input input-bordered w-full max-w-xs"
+                                            {...register("phone")}
+                                        />
+                                    </div>
+                                    <div className="form-control w-full max-w-xs">
+                                        <label className="label">
+                                            <span className="label-text">Social Link</span>
+                                        </label>
+                                        <input
+                                            type="socialLink"
+                                            placeholder="Social Link"
+                                            className="input input-bordered w-full max-w-xs"
+                                            {...register("socialLink")}
+                                        />
+                                    </div>
+                                    <input className='btn btn-primary w-full max-w-xs mt-8 text-white' type="submit" value="Add Review" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -58,9 +58,8 @@ const Login = () => {
         <div className='flex h-screen justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <h2 className="text-center text-primary text-2xl font-bold">Login</h2>
                     <form onSubmit={handleSubmit(onSubmit)}>
-
                         <div className="form-control w-full max-w-xs">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -111,7 +110,7 @@ const Login = () => {
                         </div>
 
                         {signInError}
-                        <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+                        <input className='btn btn-primary w-full max-w-xs text-white' type="submit" value="Login" />
                     </form>
                     <p className='text-center'><small>New to this site <Link className='text-primary' to="/register">Create New Account</Link></small></p>
                     <p className='text-center'><small>Forget password? <Link to='/login' onClick={resetPassword} className='text-decoration-none text-primary ms-2'>Reset Password</Link></small></p>
@@ -119,7 +118,7 @@ const Login = () => {
                     <div className="divider">OR</div>
                     <button
                         onClick={() => signInWithGoogle()}
-                        className="btn btn-outline"
+                        className="btn btn-outline btn-primary"
                     >Continue with Google</button>
                     <ToastContainer></ToastContainer>
                 </div>
