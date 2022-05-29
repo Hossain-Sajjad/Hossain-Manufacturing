712d52a2555fcfb1b8e9eae32c438515c8ba74c4
Hossain-Sajjad <sunshinems58.com>
2022-05-29 08:16:32 +0600

    review loaded

--- src/Pages/Home/Reviews/Review/Review.js
+++ src/Pages/Home/Reviews/Review/Review.js
@@ -1,20 +1,18 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../../firebase.init';
 
-const Review = () => {
+const Review = ({ review }) => {
+    const [user, loading] = useAuthState(auth);
+    const { description, ratings } = review;
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div className="card-body">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, voluptates quo incidunt harum minus mollitia!</p>
-                <div className="flex items-center">
-                    <div className="avatar">
-                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkpclgdZQ3ZHBh6xTt4wlROP30NE_GY7MdVw&usqp=CAU" alt="" />
-                        </div>
-                    </div>
-                    <div>
-                        <h4 className='text-xl'>superman</h4>
-                        <p>ratings</p>
-                    </div>
+            <div className="card-body text-center">
+                <p>{description}</p>
+                <div className="text-center">
+                    <h4 className='text-xl text-primary'>{user.displayName}</h4>
+                    <p>{ratings}</p>
+
                 </div>
             </div>
         </div>
--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Review from './Review/Review';
 
 const Reviews = () => {
+    const [review, setReview] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/review')
+            .then(res => res.json())
+            .then(data => setReview(data))
+    }, [])
     return (
-        <Review></Review>
+        <div className='my-8'>
+            <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-4'>
+                {
+                    review && review.map(r => <Review
+                        key={r._id}
+                        review={r}
+                    ></Review>)
+                }
+            </div>
+        </div>
     );
 };
 
