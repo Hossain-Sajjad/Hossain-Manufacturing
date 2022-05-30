b7617ff93b7e623c212410ddc5b11b4650142c7e
Hossain-Sajjad <sunshinems58.com>
2022-05-30 06:21:57 +0600

    all user loaded

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MakeAdmin = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/user")
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
     return (
         <div>
-            <h1>make admin page</h1>
+            <h1 className='text-2xl text-primary'>All Users</h1>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>User Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((a, index) => <tr>
+                                <td>{index + 1}</td>
+                                <td>{a.email}</td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
